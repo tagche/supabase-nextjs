b5e152b295c257ea93ce07e7cfffa4d77def546f
{tagche} <l.tagche@gmail.com>
2023-03-13 22:35:10 +0900

    css important

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   typescript: {
-    // !! 警告 !!
-    // あなたのプロジェクトに型エラーがあったとしても、プロダクションビルドを正常に完了するために危険な許可をする。
-    // !! 警告 !!
     ignoreBuildErrors: true
   }
 }
